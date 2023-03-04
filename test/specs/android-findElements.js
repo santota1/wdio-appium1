57ad38bfd2821cbc592f250a2927172a8210ef8d
santota1 <santosh.tatikonda@outlook.com>
2023-03-04 15:52:44 -0800

    Find Element By XPath

--- test/specs/android-findElements.js
+++ test/specs/android-findElements.js
@@ -5,4 +5,19 @@ describe('Android Elements Tests', () => {
         const actionBar = await $('~Action Bar')
         await expect(actionBar).toBeExisting();
     })
+
+    it('FindElementByClassName', async() => {
+        const className = await $('android.widget.TextView');
+        console.log(className.getText());
+        await expect(className).toHaveText('API Demos')
+    })
+
+    it('FindElementByXPath', async() => {
+        await $('//android.widget.TextView[@content-desc="Alert Dialogs"]').click();
+        await $('//android.widget.Button[@resource-id="io.appium.android.apis:id/select_button"]').click();
+        await $('//android.widget.TextView[@text="Command two"]').click();
+        const command2Text = await $('//android.widget.TextView[@text="You selected: 1 , Command two"]');
+        await expect(command2Text).toHaveText("You selected: 1 , Command two");
+
+    })
 });
